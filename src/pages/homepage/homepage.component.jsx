2c48c3334001442d4065a29172d50ee3992b5e25
Ritheesh <ritheesh.4@gmail.com>
2020-10-26 22:40:54 +0530

    grid added

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./homepage.styles.scss";
+import  CardsContainer  from "../../components/cards-container/cards-container.component";
 
-const HomePage = () => <div className="homepage">hiee</div>;
+const HomePage = () => (
+  <div className="homepage">
+    <CardsContainer />
+  </div>
+);
 
 export default HomePage;
