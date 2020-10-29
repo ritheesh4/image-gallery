cf8f685d50872507902c2a15998806684e3e7ed2
Ritheesh <ritheesh.4@gmail.com>
2020-10-29 22:39:56 +0530

    added nav materila ui

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./homepage.styles.scss";
 import  CardsContainer  from "../../components/cards-container/cards-container.component";
+import PrimarySearchAppBar from '../../components/nav-bar/nav-bar.component';
 
 const HomePage = () => (
   <div className="homepage">
-    <CardsContainer />
+    <PrimarySearchAppBar/>
+    <CardsContainer />    
   </div>
 );
 
