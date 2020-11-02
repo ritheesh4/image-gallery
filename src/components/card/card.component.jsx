aa23ee61bdda8a44622a9f33b8e079d277d8666d
Ritheesh <ritheesh.4@gmail.com>
2020-11-02 19:29:40 +0530

    delete and modal added

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,122 +1,122 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import CardBody from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import Grid from "@material-ui/core/Grid";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CardBody from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import Grid from '@material-ui/core/Grid'
 const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-    flexGrow: 1,
-  },
-  media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
-  },
-}));
+	root: {
+		maxWidth: 345,
+		flexGrow: 1,
+	},
+	media: {
+		height: 0,
+		paddingTop: '56.25%', // 16:9
+	},
+}))
 
 export default function Card() {
-  const classes = useStyles();
-  return (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      spacing={3}
-      style={{ width: "95vw", margin: "auto", minHeight: "83.4vh" }}
-    >
-      <Grid item xs={4}>
-        <CardBody className={classes.root}>
-          {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-          <CardMedia
-            className={classes.media}
-            image="https://homepages.cae.wisc.edu/~ece533/images/peppers.png"
-            title="Paella dish"
-          />
-        </CardBody>
-      </Grid>
-      <Grid item xs={4}>
-        <CardBody className={classes.root}>
-          {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-          <CardMedia
-            className={classes.media}
-            image="https://homepages.cae.wisc.edu/~ece533/images/peppers.png"
-            title="Paella dish"
-          />
-        </CardBody>
-      </Grid>
-      <Grid item xs={4}>
-        <CardBody className={classes.root}>
-          {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-          <CardMedia
-            className={classes.media}
-            image="https://homepages.cae.wisc.edu/~ece533/images/peppers.png"
-            title="Paella dish"
-          />
-        </CardBody>
-      </Grid>
+	const classes = useStyles()
+	return (
+		<Grid
+			container
+			direction='row'
+			justify='center'
+			alignItems='center'
+			spacing={3}
+			style={{ width: '95vw', margin: 'auto', minHeight: '83.4vh' }}
+		>
+			<Grid item xs={4}>
+				<CardBody className={classes.root}>
+					{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+					<CardMedia
+						className={classes.media}
+						image='https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+						title='Paella dish'
+					/>
+				</CardBody>
+			</Grid>
+			<Grid item xs={4}>
+				<CardBody className={classes.root}>
+					{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+					<CardMedia
+						className={classes.media}
+						image='https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+						title='Paella dish'
+					/>
+				</CardBody>
+			</Grid>
+			<Grid item xs={4}>
+				<CardBody className={classes.root}>
+					{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+					<CardMedia
+						className={classes.media}
+						image='https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+						title='Paella dish'
+					/>
+				</CardBody>
+			</Grid>
 
-      <Grid item xs={4}>
-        <CardBody className={classes.root}>
-          {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-          <CardMedia
-            className={classes.media}
-            image="https://homepages.cae.wisc.edu/~ece533/images/peppers.png"
-            title="Paella dish"
-          />
-        </CardBody>
-      </Grid>
-      <Grid item xs={4}>
-        <CardBody className={classes.root}>
-          {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-          <CardMedia
-            className={classes.media}
-            image="https://homepages.cae.wisc.edu/~ece533/images/peppers.png"
-            title="Paella dish"
-          />
-        </CardBody>
-      </Grid>
-      <Grid item xs={4}>
-        <CardBody className={classes.root}>
-          {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-          <CardMedia
-            className={classes.media}
-            image="https://homepages.cae.wisc.edu/~ece533/images/peppers.png"
-            title="Paella dish"
-          />
-        </CardBody>
-      </Grid>
+			<Grid item xs={4}>
+				<CardBody className={classes.root}>
+					{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+					<CardMedia
+						className={classes.media}
+						image='https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+						title='Paella dish'
+					/>
+				</CardBody>
+			</Grid>
+			<Grid item xs={4}>
+				<CardBody className={classes.root}>
+					{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+					<CardMedia
+						className={classes.media}
+						image='https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+						title='Paella dish'
+					/>
+				</CardBody>
+			</Grid>
+			<Grid item xs={4}>
+				<CardBody className={classes.root}>
+					{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+					<CardMedia
+						className={classes.media}
+						image='https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+						title='Paella dish'
+					/>
+				</CardBody>
+			</Grid>
 
-      <Grid item xs={4}>
-        <CardBody className={classes.root}>
-          {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-          <CardMedia
-            className={classes.media}
-            image="https://homepages.cae.wisc.edu/~ece533/images/peppers.png"
-            title="Paella dish"
-          />
-        </CardBody>
-      </Grid>
-      <Grid item xs={4}>
-        <CardBody className={classes.root}>
-          {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-          <CardMedia
-            className={classes.media}
-            image="https://homepages.cae.wisc.edu/~ece533/images/peppers.png"
-            title="Paella dish"
-          />
-        </CardBody>
-      </Grid>
-      <Grid item xs={4}>
-        <CardBody className={classes.root}>
-          {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-          <CardMedia
-            className={classes.media}
-            image="https://homepages.cae.wisc.edu/~ece533/images/peppers.png"
-            title="Paella dish"
-          />
-        </CardBody>
-      </Grid>
-    </Grid>
-  );
+			<Grid item xs={4}>
+				<CardBody className={classes.root}>
+					{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+					<CardMedia
+						className={classes.media}
+						image='https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+						title='Paella dish'
+					/>
+				</CardBody>
+			</Grid>
+			<Grid item xs={4}>
+				<CardBody className={classes.root}>
+					{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+					<CardMedia
+						className={classes.media}
+						image='https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+						title='Paella dish'
+					/>
+				</CardBody>
+			</Grid>
+			<Grid item xs={4}>
+				<CardBody className={classes.root}>
+					{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+					<CardMedia
+						className={classes.media}
+						image='https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+						title='Paella dish'
+					/>
+				</CardBody>
+			</Grid>
+		</Grid>
+	)
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import './homepage.styles.scss'
-import CardsContainer from '../../components/cards-container/cards-container.component'
-import PrimarySearchAppBar from '../../components/nav-bar/nav-bar.component'
+import React from "react";
+import "./homepage.styles.scss";
+import CardsContainer from "../../components/cards-container/cards-container.component";
+import PrimarySearchAppBar from "../../components/nav-bar/nav-bar.component";
+import Modal from "../../components/modal/modal.component";
 
 const HomePage = () => (
-	<div className='homepage'>
-		<PrimarySearchAppBar />
-		<CardsContainer />
-	</div>
-)
+  <div className="homepage">
+    <PrimarySearchAppBar />
+    {/* <CardsContainer /> */}
+    <Modal />
+  </div>
+);
 
-export default HomePage
+export default HomePage;
