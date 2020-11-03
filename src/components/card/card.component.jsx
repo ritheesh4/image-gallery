2bf6731772c9c8f9f1addabf3d48a59c8658d0c2
Ritheesh <ritheesh.4@gmail.com>
2020-11-03 11:55:41 +0530

    fixing thunk error

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import CardBody from '@material-ui/core/Card'
