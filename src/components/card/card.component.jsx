cf2d0cd9dd2cd8775afbc52ca65a354e7e46b818
Ritheesh <ritheesh.4@gmail.com>
2020-11-03 07:35:23 +0530

    linting errors fixed with eslint

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,41 +1,43 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Link from "@material-ui/core/Link";
-import CardBody from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import Grid from "@material-ui/core/Grid";
-import "./card.styles.scss";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CardBody from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import Grid from '@material-ui/core/Grid'
+import './card.styles.scss'
 const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-    flexGrow: 1,
-  },
-  media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
-  },
-}));
+	root: {
+		maxWidth: 345,
+		flexGrow: 1,
+	},
+	media: {
+		height: 0,
+		paddingTop: '56.25%', // 16:9
+	},
+}))
 
 export default function Card() {
-  const classes = useStyles();
-  return (
-    <Grid item xs={4}>
-      <CardBody className={classes.root}>
-        {/* <Grid container direction="row" justify="center" alignItems="center"> */}
-        <CardMedia
-          className={classes.media}
-          image="http://i.huffpost.com/gen/749263/original.jpg"
-          title="Paella dish"
-          onClick={(e) => {
-            e.preventDefault();
-            window.location.href = "/#dog";
-          }}
-        />
-      </CardBody>
-      <div class="lightbox-target" id="dog">
-        <img src="http://i.huffpost.com/gen/749263/original.jpg" />
-        <a class="lightbox-close" href="#"></a>
-      </div>
-    </Grid>
-  );
+	const classes = useStyles()
+	return (
+		<Grid item xs={4}>
+			<CardBody className={classes.root}>
+				{/* <Grid container direction="row" justify="center" alignItems="center"> */}
+				<CardMedia
+					className={classes.media}
+					image='http://i.huffpost.com/gen/749263/original.jpg'
+					title='Paella dish'
+					onClick={(e) => {
+						e.preventDefault()
+						window.location.href = '/#dog'
+					}}
+				/>
+			</CardBody>
+			<div className='lightbox-target' id='dog'>
+				<img
+					src='http://i.huffpost.com/gen/749263/original.jpg'
+					alt='testing'
+				/>
+				<a className='lightbox-close' href='#' />
+			</div>
+		</Grid>
+	)
 }
--- src/components/cards-container/cards-container.component.jsx
+++ src/components/cards-container/cards-container.component.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Card from "../card/card.component";
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-    flexGrow: 1,
-  },
-  media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
-  },
-}));
+import React from 'react'
+// import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Card from '../card/card.component'
+// const useStyles = makeStyles((theme) => ({
+// 	root: {
+// 		maxWidth: 345,
+// 		flexGrow: 1,
+// 	},
+// 	media: {
+// 		height: 0,
+// 		paddingTop: '56.25%', // 16:9
+// 	},
+// }))
 
 export default function CardContainer() {
-  const classes = useStyles();
-  return (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      spacing={3}
-      style={{ width: "95vw", margin: "auto", minHeight: "83.4vh" }}
-    >
-      <Card />
-    </Grid>
-  );
+	// const classes = useStyles()
+	return (
+		<Grid
+			container
+			direction='row'
+			justify='center'
+			alignItems='center'
+			spacing={3}
+			style={{ width: '95vw', margin: 'auto', minHeight: '83.4vh' }}
+		>
+			<Card />
+		</Grid>
+	)
 }
